a172724265d47478e0be0067d31910c357de9f03
Mayank Grover <49029679+MayankG514@users.noreply.github.com>
2025-04-11 03:45:35 +0530

    Add files via upload

---
+++ Application/EDA.py
@@ -0,0 +1,160 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import boto3
+from io import StringIO
+
+import boto3
+import pandas as pd
+from io import BytesIO
+
+
+AWS_ACCESS_KEY="ASIARMWYWT5WXMIH756U"
+AWS_SECRET_KEY="HxmXPjM255nectVrgPIusFgiJgOHCYeC1YFnJrfP"
+aws_session_token="IQoJb3JpZ2luX2VjEPH//////////wEaCXVzLXdlc3QtMiJHMEUCIF0oIvMP4z8Tk7CVEEZ95fJVHpo3Ph5xWtERLDDBN13MAiEAy29ggiROBkDp7gs6VAz3HiTcqT3Uq6cHvLGgp+nw/E4qqgIIahABGgwwOTYwMTc0ODk3NzMiDBL4sfBBqi9YNSyHVSqHAg+d/H2bLQJd6XfuIC59iSDbcQIAViVnG8c5PgNqy2iXY/KF2ec8UoJMcb3FIxMVrKWZV1hhf3Cekn5d2lA/simQkRYoBvkQokhILQRSPWVJ7xkUJGsISqusTrd63cwcRVLocpt2dHL7sEYvQ3vFtfsWlqehyI6InimfQ0sSGHStqczJeaU3ZHLcG1uOa70xdLZavWYiHbOCK9KqqA/fRczBPc6ktGIng9j4cckGxfmwLG1baiC5FIJiVpGbGOMVFa5IrCOEZsEXvNcE56OPAlTgR3x5SCJGwknVwErkQSnHPd7yFybBMXAmQMD1dA34ZSTNpIyWG9ZIUpN+qh4C9drRV4OIf5CBMLja0b8GOp0B8qr4Gn9wNJQiBqA1OWjxq2Hz77yit0M5yBkI2HKmM72/JpnIlJR+i9QQ31F13jNxMiAL0AXCBuoTyj8QiC+JEa/1/ucaQTiIcFNsn2npOJQ1q/G5yHMQMAau2oeEzs1CA3/s1J4XzY6r9CgRZBV8X103yvY0vh7SIj1GE7ku57zYsByVBTFGD/qvm+gj8W5ZKEHJSsV0dqoiDds7AA=="
+
+
+s3_session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    aws_session_token=aws_session_token
+)
+
+s3 = s3_session.client('s3')
+csv_obj = s3.get_object(Bucket='vapewatchers-2025', Key='marketing_image_processed.csv')
+body = csv_obj['Body'].read()
+
+df = pd.read_csv(BytesIO(body))
+
+
+# s3_path = f"s3://vape-watchers-2025/marketing_image_processed.csv"
+# df = pd.read_csv(s3_path)
+
+
+# dataset_path = "static/marketing_image_processed.csv"
+
+# Define the static image folder
+visualization_folder = "static/visualization_images"
+
+# Delete old images
+if os.path.exists(visualization_folder):
+    for file in os.listdir(visualization_folder):
+        file_path = os.path.join(visualization_folder, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+else:
+    os.makedirs(visualization_folder)
+
+# # Load the dataset
+# df = pd.read_csv(dataset_path)
+
+# Quick overview
+print(df.info())
+df.head()
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(8, 5))
+sns.countplot(data=df, x='Brightness_Level')
+plt.title('Distribution of Brightness Levels')
+plt.xlabel('Brightness Level')
+plt.ylabel('Count')
+image_path = os.path.join(visualization_folder, "Distribution_of_Brightness_Levels.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(8, 5))
+sns.countplot(data=df, x='Dominant_VIBGYOR', order=df['Dominant_VIBGYOR'].value_counts().index)
+plt.title('Dominant VIBGYOR Color Distribution')
+plt.xlabel('Dominant Color')
+plt.ylabel('Count')
+image_path = os.path.join(visualization_folder, "Dominant_VIBGYOR_Color_Distribution.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(6, 4))
+sns.countplot(data=df, x='Cartoon')
+plt.title('Presence of Cartoon Elements in Images')
+plt.xlabel('Cartoon')
+plt.ylabel('Count')
+image_path = os.path.join(visualization_folder, "Presence_of_Cartoon_Elements_in_Images.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(6, 4))
+sns.countplot(data=df, x='Face')
+plt.title('Presence of Human Faces in Images')
+plt.xlabel('Face Detected')
+plt.ylabel('Count')
+image_path = os.path.join(visualization_folder, "Presence_of_Human_Faces_in_Images.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(8, 5))
+sns.countplot(data=df[df['Vape_Type'].notnull()], x='Vape_Type', order=df['Vape_Type'].value_counts().index)
+plt.title('Vape Type Distribution')
+plt.xlabel('Vape Type')
+plt.ylabel('Count')
+plt.xticks(rotation=15)
+image_path = os.path.join(visualization_folder, "Vape_Type_Distribution.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(10, 5))
+sns.countplot(data=df[df['Age'].notnull()], x='Age',
+              order=df['Age'].value_counts().index)
+plt.title('Age Distribution in Images')
+plt.xlabel('Age Group')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+image_path = os.path.join(visualization_folder, "Age_Distribution_in_Images.png")
+plt.savefig(image_path)
+plt.close()
+
+pivot_brightness = pd.crosstab(df['Vape_Type'], df['Brightness_Level'])
+plt.figure(figsize=(10, 6))
+sns.heatmap(pivot_brightness, annot=True, fmt='d', cmap='YlGnBu')
+plt.title('Heatmap of Vape Type vs Brightness Level')
+plt.xlabel('Brightness Level')
+plt.ylabel('Vape Type')
+image_path = os.path.join(visualization_folder, "VapeType_vs_BrightnessLevel.png")
+plt.savefig(image_path)
+plt.close()
+
+cartoon_color_ct = pd.crosstab(df['Dominant_VIBGYOR'], df['Cartoon'])
+cartoon_color_ct.plot(kind='bar', stacked=True, figsize=(10, 6))
+plt.title('Stacked Bar Chart: Cartoon Presence by Dominant Color')
+plt.xlabel('Dominant VIBGYOR Color')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend(title='Cartoon')
+plt.tight_layout()
+image_path = os.path.join(visualization_folder, "Stacked Bar Chart.png")
+plt.savefig(image_path)
+plt.close()
+
+plt.figure(figsize=(8, 6))
+sns.violinplot(data=df, x='Face', y='Brightness_Level', palette='muted')
+plt.title('Brightness Level Distribution by Face Presence')
+plt.xlabel('Face Detected')
+plt.ylabel('Brightness Level')
+image_path = os.path.join(visualization_folder, "BrightnessLevelDistribution.png")
+plt.savefig(image_path)
+plt.close()
+
+
+# # Create a cross-tabulation of Cartoon vs Company
+# cartoon_company_ct = pd.crosstab(df['Company'], df['Cartoon'])
+
+# # Plot the stacked bar chart
+# cartoon_company_ct.plot(kind='bar', stacked=True, figsize=(12, 6), colormap='Set2')
+# plt.title('Stacked Bar Chart: Cartoon vs Non-Cartoon Images by Company')
+# plt.xlabel('Company')
+# plt.ylabel('Number of Images')
+# plt.xticks(rotation=45)
+# plt.legend(title='Cartoon')
+# plt.tight_layout()
+# image_path = os.path.join(visualization_folder, "Cartoon_vs_NonCartoon.png")
+# plt.savefig(image_path)
+# plt.close()
---
+++ Application/Labeling.py
@@ -0,0 +1,19 @@
+import pandas as pd
+def label_data(row):
+    if row['Face'] == 1 and row['Age'] in ['(0-2)', '(4-6)', '(8-13)', '(15-20)', '(25-32)']:
+        return 1
+    
+    elif row['Cartoon'] == 1:
+        return 1
+    
+    elif row['Brightness_Level'] > 5:
+        return 1
+    
+    elif row['Dominant_VIBGYOR'] in ['Red', 'Orange', 'Yellow']:
+        return 1
+    return 0
+
+df = pd.read_csv('static/marketing_images_processed.csv')
+df['Label'] = df.apply(label_data, axis=1)
+
+print(df.head())
